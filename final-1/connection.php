551d1edda348964503fe4bdf8f185b5f3d652c57
NipunaCK <kanc_karunanayaka@std.geo.sab.ac.lk>
2022-04-25 14:15:24 +0530

    dhd66

--- final-1/connection.php
+++ final-1/connection.php
@@ -2,7 +2,7 @@
     $host = "localhost";  
     $user = "root";  
     $password = '';  
-    $db_name = "pasan";  
+    $db_name = "pasan1";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
