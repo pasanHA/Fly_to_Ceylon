6c0dc5c535d0bf5f425a8ca6c58f17d5a31d0bd0
NipunaCK <kanc_karunanayaka@std.geo.sab.ac.lk>
2022-04-29 11:25:17 +0530

    A

--- latest travele/admin/Add destination.php
+++ latest travele/admin/Add destination.php
@@ -1,15 +1,14 @@
-<?php require_once('connection.php'); ?>
-<?php
+<?php require_once('connection.php');
 if(isset($_POST['submit']))
 {
  $destination=$_POST['destination'];
 $description=$_POST['discription'];
-$city=$_POST['city'];
+//$city=$_POST['city'];
 // $fclass=$_POST['flight_class'];
 // $additional=$_POST['additional'];
 
-$query="INSERT INTO destination (Destination_Name,City,Discription)
-VALUES('{$destination}','{$city}','{$description}')
+$query="INSERT INTO destination (Destination_Name,Discription)
+VALUES('{$destination}','{$description}')
 LIMIT 1";
 
 $result=mysqli_query($connect,$query);
